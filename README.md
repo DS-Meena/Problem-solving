8c0f865eb9d300995c659134cb2027c87e2fec58
DS-Meena <dharamsinghmeena2000@gmail.com>
2024-09-27 10:22:53 +0530

    attempted 9 problems

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Problem-solving
 This repository is my problem solving journal. Will contain the learnings gained from solving coding problems.
+
+## Mathematics
+
+Sum of AP:
+
+$sum = \frac{len}{2} * (\text{first term} + \text{last term})$
--- test.cpp
+++ test.cpp
@@ -1,31 +1,81 @@
 #include <bits/stdc++.h>
-
+    
 using namespace std;
 #define ll long long
+#define p pair<int, ll>
+
+void dijkstra(int source, vector<vector<ll>> &d, vector<vector<p>> &graph, vector<bool> &hs) {
+
+    // comparator for set
+    auto cmp = [&](auto &a, auto &b) {
+        return make_pair(d[a.first][a.second], a) < 
+                make_pair(d[b.first][b.second], b);
+    };
+
+    set<pair<int, int>, decltype(cmp)> mnHeap(cmp);
+
+    d[source][0] = 0;
+    mnHeap.insert({source, 0});
+
+    while(!mnHeap.empty()) {
+
+        auto [u, withHorse] = *mnHeap.begin();
+        mnHeap.erase(mnHeap.begin());
+
+        // do we have a horse
+        bool horse = (withHorse || hs[u]);
+
+        for (auto &[v, w] : graph[u]) {
+
+            ll dist = horse ? w/2 : w;
+            if (d[v][horse] > d[u][withHorse] + dist) {
+                
+                mnHeap.erase({v, horse});
+                d[v][horse] = d[u][withHorse] + dist;
+
+                mnHeap.insert({v, horse});
+            }
+        }
+    }
+}
 
 int main() {
     
     int t; cin >> t;
     while(t-->0) {
         
-        int n, d, k; cin >> n >> d >> k;
-        vector<int> l(k), r(k);
-        for (int i=0; i<k; i++) {
-            cin >> l[i] >> r[i];
-        }
-        
-        // apply prefix sum 
-        vector<int> psum(n+2, 0);
-        for (int i=0; i<k; i++) {
-            psum[l[i]]++;
-            psum[r[i]+1]--;
-        }   
+        int n, m, h; cin >> n >> m >> h;
 
-        for (int i=2; i<=n+1; i++) {
-            psum[i] += psum[i-1];
+        vector<bool> hs(n);
+        vector<vector<p>> graph(n);
+
+        for (int i=0; i<h; i++) {
+            int x; cin >> x;
+            hs[--x] = 1;
         }
 
-        // sliding window - find window with max maximum value
-        
+        for (int i=0; i<m; i++) {
+            int u, v, w; cin >> u >> v >> w;
+            
+            u--; v--;
+            graph[u].push_back({v, w});
+            graph[v].push_back({u, w});
+        }
+
+        vector<vector<ll>> d1(n, vector<ll>(2, 1e18));
+        vector<vector<ll>> d2(n, vector<ll>(2, 1e18));
+
+        dijkstra(0, d1, graph, hs);
+        dijkstra(n-1, d2, graph, hs);
+
+        ll best = 1e18;
+        // lambda function
+        auto get = [&](int a) {return max(min(d1[a][0], d1[a][1]), min(d2[a][0], d2[a][1]));};
+
+        for (int i=0; i<n; i++) {
+            best = min(best, get(i));
+        }
+
+        cout << (best == 1e18 ? -1 : best) << endl;
     }
 }
